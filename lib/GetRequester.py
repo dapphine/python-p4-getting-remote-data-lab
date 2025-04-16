9f0dc7c530cc35a1e615930eb44d7d1b85d205c0
dapphine <dapphine.michere@student.moringaschool.com>
2025-04-16 16:54:43 +0300

    final commit

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,9 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content  # Return the response body as bytes
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        return json.loads(response_body)  # Parse and return the JSON data
